a6f2a9a9ef86e801226243a671080a07fb949776
sirinebenabdesslem55 <sirinebenabdesslem55@gmail.com>
2020-03-10 20:55:52 +0100

    mis à jour api

--- views/common/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="site-footer">
-      <div class="text-center">
-        <p>
-          &copy; Copyrights <strong>EBrigade</strong>. All Rights Reserved
-        </p>
-      </div>
-</footer>
